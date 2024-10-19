f744d17d99fdc505d38e78a721c5820e4fc11289
jac00000b <118233742+jac00000b@users.noreply.github.com>
2024-10-19 19:45:48 +0800

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import { routeTree } from "./routeTree.gen";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
