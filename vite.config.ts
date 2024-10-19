f62be8652e9e608afedfece8dff801721d0e83f8
jac00000b <118233742+jac00000b@users.noreply.github.com>
2024-10-19 21:44:46 +0800

    use splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
