512fae887e5e20061b530e975807067aca0e7efc
jac00000b <118233742+jac00000b@users.noreply.github.com>
2024-10-19 23:18:21 +0800

    add vite-plugin-html

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import path from "path";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { dohServers } from "./src/lib/utils";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), splitVendorChunkPlugin()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    splitVendorChunkPlugin(),
+    createHtmlPlugin({
+      minify: true,
+      entry: "src/main.tsx",
+      inject: {
+        tags: dohServers.map((server) => ({
+          injectTo: "head",
+          tag: "link",
+          attrs: {
+            rel: "dns-prefetch",
+            href: new URL(server.url).origin,
+          },
+        })),
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
